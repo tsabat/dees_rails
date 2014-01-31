750d15dc1c08e5052b85871b5767a2171a4588ae
Timothy Sabat <timothy.sabat@gmail.com>
2014-01-30 21:58:43 -0800

    more stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.1.0'
 
 set :application, 'test'
-set :repo_url, 'git@github.com:tsabat/dees_rails.git'
+set :repo_url, 'https://github.com/tsabat/dees_rails.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
@@ -40,7 +40,7 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
