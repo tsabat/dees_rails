2ff9f7edca07629d3b13167702097213e3606062
Timothy Sabat <timothy.sabat@gmail.com>
2014-01-30 21:32:43 -0800

    some cap stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,16 +2,16 @@
 lock '3.1.0'
 
 set :application, 'test'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'git@github.com:tsabat/dees_rails.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+ set :deploy_to, '/home/ubuntu'
 
 # Default value for :scm is :git
-# set :scm, :git
+ set :scm, :git
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -32,7 +32,7 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+ set :keep_releases, 5
 
 namespace :deploy do
 
